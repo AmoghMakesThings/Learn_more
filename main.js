8aa78f9417880a27d58e5c4950c229c180773b08
AmoghMakesThings <85697969+AmoghMakesThings@users.noreply.github.com>
2021-06-12 09:03:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+function test() {
+    document.getElementById("test").src="https://i.pinimg.com/originals/27/2a/78/272a78f13f381ab7bdf2541227edf218.jpg"
+}
+function test2(){
+    document.getElementById("test").src="https://i.pinimg.com/originals/39/42/a1/3942a180299d5b9587c2aa8e09d91ecf.jpg"
+}
+function test3() {
+    document.getElementById("img2").src="https://i.pinimg.com/originals/4b/5d/c9/4b5dc99a46f7375c8ece039453ab26cf.png"
+}
+function test4() {
+    document.getElementById("img2").src="https://i.pinimg.com/originals/23/a3/5b/23a35bc2732724b86542f2d7db8d227c.jpg"
+}
+function test5() {
+    document.getElementById("img3").src="https://i.pinimg.com/originals/cd/43/7c/cd437c8f34f195592f30bb7d604bbf01.jpg"
+}
+function test6() {
+    document.getElementById("img3").src="https://i.pinimg.com/originals/84/b2/f9/84b2f9b5d63894089471dd150f5c101c.jpg"
+}
+function test7() {
+    document.getElementById("mainthing").src="di-art2.png"
+}
+function test8() {
+    document.getElementById("mainthing").src="di-art1.png"
+}
